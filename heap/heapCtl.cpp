ee5b688fb11eba2b2d986a3827ff27d4ab0481c2
杨智康 <55861791+DribbleLife11@users.noreply.github.com>
2022-02-21 21:00:07 +0800

    堆操作
    堆实际上是用数组实现的完全二叉树

---
+++ heap/heapCtl.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+void heapInsert(vector<int>& nums, int index) {
+    while (nums[index] > nums[(index - 1) / 2]) {
+        swap(nums[index], nums[(index - 1) /2]);
+        index = (index - 1) / 2;
+} 
+    
+void heapify(vector<int>& nums, int index, int heapSize) {
+    int left = index * 2 + 1;       //左孩子下标
+    while (left < heapSize) {       //当有孩子时
+        int largest = left + 1 < heapSize && nums[left] < nums[left + 1] ? left + 1 : left;     //两个孩子哪个值最大，就把谁的下标给largest
+        
+        largest = nums[largest] > nums[index] ? largest : index;        //父节点和最大的孩子进行值的比较，哪个大就把谁的下标给largest
+        if (largest == index)   break;
+        
+        swap(nums[largest], nums[index]);
+        index = largest;
+        left = index * 2 + 1;
+        
+}
--- heap/heapInsert.cpp
+++
@@ -1,6 +0,0 @@
-#include <vector>
-void heapInsert(vector<int>& nums, int index) {
-    while (nums[index] > nums[(index - 1) / 2]) {
-        swap(nums[index], nums[(index - 1) /2]);
-        index = (index - 1) / 2;
-} 
