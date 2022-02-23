7019c4788a903e6035f2b0177001ab70ce0db145
杨智康 <55861791+DribbleLife11@users.noreply.github.com>
2022-02-23 11:23:58 +0800

    快速排序

--- sort/QuickSort.cpp
+++ sort/QuickSort.cpp
@@ -19,11 +19,36 @@
     
 /
 #include <vector>
-
-int[] partition(int arr[], int l, int r) {
-    
+vector<int> partition(vector<int>& nums, int l, int r) {
+	int less = l - 1;	//<区域边界
+	int more = r;		//>区域边界
+	while (l < more) {
+		if (nums[l] < nums[r]) {
+			swap(nums[++less], nums[l++]);
+		}
+		else if (nums[l] > nums[r]) {
+			swap(nums[--more], nums[l]);
+		}
+		else {
+			l++;
+		}
+	}
+	swap(nums[more], nums[r]);
+	return { less + 1, more };
 }
 
-void quickSort() {
-     
+void quickSort(vector<int> nums, int l, int r) {
+	if (l < r) {
+		swap(nums[l + rand() % (r - l + 1)], nums[r]);
+		vector<int> p = partition(nums, l, r);
+		quickSort(nums, l, p[0] - 1);
+		quickSort(nums, p[1] + 1, r);
+	}
+}
+
+void quickSort(vector<int> nums) {
+	if (nums.empty() || (int)nums.size() < 2) {
+		return;
+	}
+	quickSort(nums, 0, (int)nums.size() - 1);
 }
